db244c82b04f874749a99e1f8832d19bc17cea3d
Leandro Ribeiro de Souza <souza.ribeiro92@gmail.com>
2021-05-25 14:15:05 -0300

    secao 6

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      {path: 'membros', component: MemberListComponent, canActivate: [AuthGuard]},
+      {path: 'membros/:id', component: MemberDetailComponent},
+      {path: 'listas', component: ListsComponent},
+      {path: 'mensagens', component: MessagesComponent}
+    ]
+  },
+  {path: '**', component: HomeComponent, pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
